c95e9285f13739c0b305e780e48a2aaacdfee44a
RayHan904 <54216177+RayHan904@users.noreply.github.com>
2024-06-02 15:19:35 -0400

    Update init.ts

--- src/schema/init.ts
+++ src/schema/init.ts
@@ -3,36 +3,42 @@ import {pool} from "../config/sqlDb.js"
 const createTables = async () => {
     try {
       await pool.query(`
-        CREATE TABLE IF NOT EXISTS Orders (
-          clientId VARCHAR(255),
-          date TIMESTAMP,
-          totalOrders INT,
-          Canada INT,
-          US INT,
-          INTL INT,
-          avgQtyPerOrder FLOAT
-        );
-  
-        CREATE TABLE IF NOT EXISTS Customers (
-          clientId VARCHAR(255) PRIMARY KEY,
-          clientName VARCHAR(255)
-        );
-  
-        CREATE TABLE IF NOT EXISTS RegionShipped (
-          clientId VARCHAR(255),
-          date TIMESTAMP,
-          country VARCHAR(255),
-          stateProvince VARCHAR(255)
-        );
-  
-        CREATE TABLE IF NOT EXISTS SkuSales (
-          clientId VARCHAR(255),
-          retailerId INT,
-          sku VARCHAR(255),
-          date TIMESTAMP,
-          totalOrders INT,
-          totalUnits INT
-        );
+      CREATE TABLE IF NOT EXISTS Orders (
+        clientId VARCHAR(255),
+        date TIMESTAMP,
+        totalOrders INT,
+        Canada INT,
+        US INT,
+        INTL INT,
+        avgQtyPerOrder FLOAT,
+        PRIMARY KEY (clientId, date),
+        FOREIGN KEY (clientId) REFERENCES Customers(clientId)
+      );
+      
+      CREATE TABLE IF NOT EXISTS Customers (
+        clientId VARCHAR(255) PRIMARY KEY,
+        clientName VARCHAR(255)
+      );
+      
+      CREATE TABLE IF NOT EXISTS RegionShipped (
+        clientId VARCHAR(255),
+        date TIMESTAMP,
+        country VARCHAR(255),
+        stateProvince VARCHAR(255),
+        PRIMARY KEY (clientId, date),
+        FOREIGN KEY (clientId) REFERENCES Customers(clientId)
+      );
+      
+      CREATE TABLE IF NOT EXISTS SkuSales (
+        clientId VARCHAR(255),
+        retailerId INT,
+        sku VARCHAR(255),
+        date TIMESTAMP,
+        totalOrders INT,
+        totalUnits INT,
+        PRIMARY KEY (clientId, sku, date),
+        FOREIGN KEY (clientId) REFERENCES Customers(clientId)
+      );
       `);
   
       console.log('Tables created successfully');
