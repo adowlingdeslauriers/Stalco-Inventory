da5ae27205c0aed7317b44cd757200c9eb786391
RayHan904 <54216177+RayHan904@users.noreply.github.com>
2024-06-02 15:11:15 -0400

    Update init.ts

--- src/schema/init.ts
+++ src/schema/init.ts
@@ -4,7 +4,7 @@ const createTables = async () => {
     try {
       await pool.query(`
         CREATE TABLE IF NOT EXISTS Orders (
-          ClientID VARCHAR(255),
+          clientId VARCHAR(255),
           date TIMESTAMP,
           totalOrders INT,
           Canada INT,
@@ -14,19 +14,19 @@ const createTables = async () => {
         );
   
         CREATE TABLE IF NOT EXISTS Customers (
-          ClientID VARCHAR(255) PRIMARY KEY,
-          ClientName VARCHAR(255)
+          clientId VARCHAR(255) PRIMARY KEY,
+          clientName VARCHAR(255)
         );
   
         CREATE TABLE IF NOT EXISTS RegionShipped (
-          ClientID VARCHAR(255),
+          clientId VARCHAR(255),
           date TIMESTAMP,
           country VARCHAR(255),
           stateProvince VARCHAR(255)
         );
   
         CREATE TABLE IF NOT EXISTS SkuSales (
-          ClientID VARCHAR(255),
+          clientId VARCHAR(255),
           retailerId INT,
           sku VARCHAR(255),
           date TIMESTAMP,
